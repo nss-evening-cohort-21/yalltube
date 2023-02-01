7fede51c38589786c5ea7714d2b021b6eef0e907
Eric Frey <ericlfrey@gmail.com>
2023-01-31 19:40:51 -0600

    Created Basic Modal Component. Added Profile Picture to Modal

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { signOut } from '../utils/auth';
+import ProfileModal from './ProfileModal';
 
 export default function NavBar() {
   return (
@@ -28,6 +29,7 @@ export default function NavBar() {
             <button type="button" className="btn btn-danger" onClick={signOut}>
               Sign Out
             </button>
+            <ProfileModal />
           </ul>
         </div>
       </div>
---
+++ components/ProfileModal.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Image, Modal } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+
+export default function ProfileModal() {
+  const [show, setShow] = useState(false);
+  const { user } = useAuth();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      {/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button> */}
+      <Image
+        src={user.photoURL}
+        alt="Picture of the author"
+        width="50"
+        height="50"
+        onClick={handleShow}
+        roundedCircle
+      />
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Image
+            src={user.photoURL}
+            alt="Picture of the author"
+            width="50"
+            height="50"
+            onClick={handleShow}
+            roundedCircle
+          />
+        </Modal.Header>
+        <Modal.Body>Woohoo, reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          {/* <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button> */}
+          {/* <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
