0ee71c1c8cc23c57194624a43d1b6a37cf6513be
Eric Frey <ericlfrey@gmail.com>
2023-01-31 21:31:24 -0600

    Saving for a merge

--- components/ProfileModal.js
+++ components/ProfileModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Card, Image, Modal, ModalTitle,
+  Card, Image, Modal, ModalBody, ModalTitle,
 } from 'react-bootstrap';
 import Link from 'next/link';
 import { useAuth } from '../utils/context/authContext';
@@ -26,36 +26,38 @@ export default function ProfileModal() {
       />
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Image
-            src={user.photoURL}
-            alt="Picture of the author"
-            width="60"
-            height="60"
-            onClick={handleShow}
-            roundedCircle
-          />
-          <div>
-            <ModalTitle>{user.displayName}</ModalTitle>
-            <ModalTitle>{user.email}</ModalTitle>
-          </div>
-        </Modal.Header>
-        <Modal.Body>
-          <Link passHref href="/library">
-            <Card.Link href="" onClick={handleClose}>
-              Your channel
-            </Card.Link>
-          </Link>
-          <br />
-          <Link passHref href="/">
-            <Card.Link href="" onClick={signOut}>
-              Sign out
-            </Card.Link>
-          </Link>
-        </Modal.Body>
-        <Modal.Footer>
-          Last Login Date {new Date(user.metadata.lastSignInTime).toLocaleDateString()};
-        </Modal.Footer>
+        <ModalBody className="profile-modal">
+          <Modal.Header>
+            <Image
+              src={user.photoURL}
+              alt="Picture of the author"
+              width="60"
+              height="60"
+              onClick={handleShow}
+              roundedCircle
+            />
+            <div>
+              <ModalTitle>{user.displayName}</ModalTitle>
+              <ModalTitle>{user.email}</ModalTitle>
+            </div>
+          </Modal.Header>
+          <Modal.Body>
+            <Link passHref href="/library">
+              <Card.Link href="" onClick={handleClose}>
+                Your channel
+              </Card.Link>
+            </Link>
+            <br />
+            <Link passHref href="/">
+              <Card.Link href="" onClick={signOut}>
+                Sign out
+              </Card.Link>
+            </Link>
+          </Modal.Body>
+          <Modal.Footer>
+            Last Login Date {new Date(user.metadata.lastSignInTime).toLocaleDateString()};
+          </Modal.Footer>
+        </ModalBody>
       </Modal>
     </>
   );
