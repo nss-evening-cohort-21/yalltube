3de2b70f3b03d7c7747883892c02aabda9141839
GrahamJReid <grahamreidtechnology@gmail.com>
2023-02-02 19:30:09 -0600

    potential scroll fix

--- pages/library.js
+++ pages/library.js
@@ -20,10 +20,12 @@ export default function Home() {
       <Head>
         <title>Home Videos</title>
       </Head>
-      <div className="home-content-container">
-        <div className="home-card-container">{libraryVideos.map((video) => (
-          <VideoCard key={video.firebaseKey} videoObj={video} onUpdate={displayUserVideos} />
-        ))}
+      <div className="center-home-content">
+        <div className="home-content-container">
+          <div className="home-card-container">{libraryVideos.map((video) => (
+            <VideoCard key={video.firebaseKey} videoObj={video} onUpdate={displayUserVideos} />
+          ))}
+          </div>
         </div>
       </div>
     </>
