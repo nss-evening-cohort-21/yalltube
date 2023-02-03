e473d9bcddd89c598f2d3926bdb21de2e5542902
Erin Stephens <erebstephens@gmail.com>
2023-02-02 20:48:16 -0600

    added additional styles to the offcanvas

--- components/Offcanvas.js
+++ components/Offcanvas.js
@@ -12,32 +12,32 @@ export default function NavOffcanvas() {
 
   return (
     <>
-      <Button variant="danger" onClick={handleShow}>
+      <Button variant="dark" className="hamburger-btn" onClick={handleShow} size="lg">
         &#9776;
       </Button>
 
       <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title><WhiteLogo /></Offcanvas.Title>
-        </Offcanvas.Header>
+        <Link href="/" passHref>
+          <Offcanvas.Title onClick={handleClose}><WhiteLogo /></Offcanvas.Title>
+        </Link>
         <Offcanvas.Body>
           <Link href="/" passHref>
-            <Button variant="danger" type="button" size="lg" className="copy-btn m-2" onClick={handleClose}>
+            <Button variant="danger" type="button" size="lg" className="copy-btn m-2 offcanvas-btn" onClick={handleClose}>
               &#8962; Home
             </Button>
           </Link>
           <Link href="/library" passHref>
-            <Button variant="danger" type="button" size="lg" className="copy-btn m-2" onClick={handleClose}>
+            <Button variant="danger" type="button" size="lg" className="copy-btn m-2 offcanvas-btn" onClick={handleClose}>
               &#128214; Library
             </Button>
           </Link>
           <Link href="/video/new" passHref>
-            <Button variant="danger" type="button" size="lg" className="copy-btn m-2" onClick={handleClose}>
+            <Button variant="danger" type="button" size="lg" className="copy-btn m-2 offcanvas-btn" onClick={handleClose}>
               &#128249; Add a Video
             </Button>
           </Link>
           <Link href="/" passHref>
-            <Button variant="danger" type="button" size="lg" className="copy-btn m-2" onClick={handleClose}>
+            <Button variant="danger" type="button" size="lg" className="copy-btn m-2 offcanvas-btn" onClick={handleClose}>
               &#127911; Playlists
             </Button>
           </Link>
