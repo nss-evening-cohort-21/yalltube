468a64b98847e8162e354d5bc4e5da4066dc5eb0
Whitley Beers <whitleybeers@gmail.com>
2023-02-06 19:53:05 -0600

    need to test dynamic routing for edit playlist

--- pages/playlist/edit/[firebaseKey].js
+++ pages/playlist/edit/[firebaseKey].js
@@ -1,7 +1,24 @@
-import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import PlaylistForm from '../../../components/forms/PlaylistForm';
 
 export default function EditPlaylistPage() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    // TODO: GETSINGLEPLAYLIST API CALL
+    console.warn(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
+
   return (
-    <div>EditPlaylist</div>
+    <>
+      <Head>
+        <title>Edit Playlist</title>
+      </Head>
+      <PlaylistForm obj={editItem} />
+    </>
   );
 }
