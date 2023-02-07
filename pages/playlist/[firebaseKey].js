c280f06af9824c0d6969ed23ab5333d2e238730d
Eric Frey <ericlfrey@gmail.com>
2023-02-07 17:54:06 -0600

    Saving to refresh and reload. Buggy. not getting the playlist with the useEffect correctly

--- pages/playlist/[firebaseKey].js
+++ pages/playlist/[firebaseKey].js
@@ -1,10 +1,27 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSinglePlaylist } from '../../API/playlistData';
+import PlaylistVideoCard from '../../components/PlaylistVideoCard';
 
 export default function ViewPlaylistPage() {
+  const [playlist, setPlaylist] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
+
+  const getPlaylist = () => {
+    getSinglePlaylist(firebaseKey).then(setPlaylist);
+  };
+  useEffect(() => {
+    getPlaylist();
+  }, []);
   return (
-    <h1>Playlist: {firebaseKey}</h1>
+    <>
+      <Head>
+        <title>PlaYlist</title>
+      </Head>
+      {/* <h1>Playlist: {playlist.playlist_name}</h1> */}
+      <PlaylistVideoCard playlistVideoObj={playlist} onUpdate={getPlaylist} />
+    </>
   );
 }
--- pages/playlists.js
+++ pages/playlists.js
@@ -1,11 +1,12 @@
-/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { getPlaylists } from '../API/playlistData';
 import PlaylistCard from '../components/PlaylistCard';
+import Signin from '../components/Signin';
 import { useAuth } from '../utils/context/authContext';
 
 export default function PlaylistsPage() {
@@ -17,21 +18,28 @@ export default function PlaylistsPage() {
   };
   useEffect(() => {
     getAllPlaylists();
-  }, []);
+  }, [user]);
   return (
     <>
       <Head>
-        <title>{user.displayName}'s Playlists</title>
+        <title>{user ? `${user.displayName}'s Playlists` : 'Sign In'}</title>
       </Head>
-      <div className="text-center mt-4">
-        <h1 className="mb-4">{user.displayName}'s Playlists</h1>
-        <Link passHref href="./playlist/new">
-          <Button type="button">Create Playlist</Button>
-        </Link>
-      </div>
-      <div className="d-flex flex-wrap justify-content-center mt-5">
-        {playlists.map((item) => <PlaylistCard key={item.firebaseKey} playlistObj={item} onUpdate={getAllPlaylists} />)}
-      </div>
+      {user
+        ? (
+          <>
+            <div className="text-center mt-4">
+              <h1 className="mb-4">{user.displayName}'s Playlists</h1>
+              <Link passHref href="./playlist/new">
+                <Button type="button">Create Playlist</Button>
+              </Link>
+            </div>
+            <div className="d-flex flex-wrap justify-content-center mt-5">
+              {playlists.map((item) => <PlaylistCard key={item.firebaseKey} playlistObj={item} onUpdate={getAllPlaylists} />)}
+            </div>
+          </>
+        )
+        : <Signin />}
+
     </>
   );
 }
