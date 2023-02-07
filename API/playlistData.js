ecad696dad41cc9bcc8f6cbbcf0227ff44e99564
Allison Blumenthal <blumen.allison@gmail.com>
2023-02-06 20:28:06 -0500

    added mock playlist data

---
+++ API/playlistData.js
@@ -0,0 +1,18 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// GET Playlists
+
+
+// CREATE Playlist
+
+
+// EDIT Playlist
+
+
+// DELETE Playlist
+
+
+// GET Single Playlist
+
