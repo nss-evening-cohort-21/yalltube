ef4312d611545aa99b2cac39d8186ab8ce2d67c8
Eric Frey <ericlfrey@gmail.com>
2023-02-07 16:55:01 -0600

    Added Head/title and did basic styling to Playlists page

--- pages/playlists.js
+++ pages/playlists.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
+import Head from 'next/head';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
@@ -18,13 +20,16 @@ export default function PlaylistsPage() {
   }, []);
   return (
     <>
-      <div className="text-center">
-        <h1>PlaylistsPage</h1>
+      <Head>
+        <title>{user.displayName}'s Playlists</title>
+      </Head>
+      <div className="text-center mt-4">
+        <h1 className="mb-4">{user.displayName}'s Playlists</h1>
         <Link passHref href="./playlist/new">
           <Button type="button">Create Playlist</Button>
         </Link>
       </div>
-      <div>
+      <div className="d-flex flex-wrap justify-content-center mt-5">
         {playlists.map((item) => <PlaylistCard key={item.firebaseKey} playlistObj={item} onUpdate={getAllPlaylists} />)}
       </div>
     </>
